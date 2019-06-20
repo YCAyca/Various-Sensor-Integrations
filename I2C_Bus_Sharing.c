8aca70c944dc39c15817918b7acbe5e1fa58e95d
YCAyca <aktas.yagmur@gmail.com>
2019-06-20 16:58:32 +0300

    Update I2C_Bus_Sharing.c

--- I2C_Bus_Sharing.c
+++ I2C_Bus_Sharing.c
@@ -1,15 +1,88 @@
-/* It's the source code of I2C_Bus_Sharing of these embedded system. 
+/*  It's the source code of I2C_Bus_Sharing of these embedded system. 
+
+    I2C_Task() function make transmit data in microprossor -> component & component -> microprossor ways. 
+    The function waits in I2C_START case until the first data transfer in the bus progressed. When the transmission 
+    starts, the function passes to the I2C_TRANSMIT case. When the related data is written in the related register 
+    of a sensor or the data is read from related register, the function enters in I2C_DONE case. It means the bus is 
+    free now.
    
-   In these purpose, I2C_Done() function prevents that another sensor from who having the bus, uses the bus, 
-   until it finishes its work in this system tour (transmits the necessary information to the microprocessor and 
-   receives the necessary answers). This prevents the sensors from interrupting each other's communication. 
-   If the bus is idle, the function returns true so that the next sensor receives the bus. So all sensors use the bus in sequence.
+    I2C_Done() function prevents that another sensor from who having the bus, uses the bus, 
+    until it finishes its work in this system tour (transmits the necessary information to the microprocessor and 
+    receives the necessary answers). This prevents the sensors from interrupting each other's communication. 
+    If the bus is idle, the function returns true so that the next sensor receives the bus. So all sensors use the bus in sequence.
 
     In this platform there is a IOEX Driver (who controls motors), a LED Driver(who controls leds) an Accelerometer & Gyro Sensor and a 
     proxy sensor who needs to use I2C Bus for data transfering with microprocessor. Therefore the Run_System_Components() function
     make these sensors use I2C Bus sequentially with the help of I2C_Done() function. When a component have the bus, it start to use its
     own task function to program itself in the right way. The task functions is decribed in Components_Task_Function.c file
 */
+
+void I2C_Task(void)
+{		
+	switch(i2cStates)
+	{
+		case I2C_START:
+        {
+            // Switch to the Transmit State.
+            i2cStates = I2C_TRANSMIT;      
+			break;
+		}	
+		case I2C_TRANSMIT:
+        {
+            // If no transmission has occured yet...
+            if(mikropData.I2CBufferHandle == NULL)
+            {
+                mikropData.I2CBufferHandle = DRV_I2C_Transmit(mikropData.handleI2C0,  mikropSlaveAddress, I2CWriteString, I2C_TX_SIZE, NULL);
+            } 
+            else
+            {
+                mikropData.I2CBufferEvent = DRV_I2C_TransferStatusGet ( mikropData.handleI2C0,mikropData.I2CBufferHandle );
+                if(mikropData.I2CBufferEvent == DRV_I2C_BUFFER_EVENT_COMPLETE) 
+                {
+                    i2cStates = I2C_DONE;
+                    mikropData.I2CBufferHandle = NULL;
+                }
+                if(mikropData.I2CBufferEvent == DRV_I2C_BUFFER_EVENT_ERROR)
+                {
+                    i2cStates = I2C_ERROR;
+                }
+            }
+
+            break;
+		}
+        case I2C_TRANSMITTHENRECEIVE:
+        {
+            // If no transmission has occured yet...
+            if(mikropData.I2CBufferHandle == NULL)
+            {
+                mikropData.I2CBufferHandle = DRV_I2C_TransmitThenReceive ( mikropData.handleI2C0,  mikropSlaveAddress, I2CWriteString, I2C_TX_SIZE, I2CReadString, I2C_RX_SIZE, NULL);
+            } 
+            else
+            {
+                mikropData.I2CBufferEvent = DRV_I2C_TransferStatusGet ( mikropData.handleI2C0,mikropData.I2CBufferHandle );
+                if(mikropData.I2CBufferEvent == DRV_I2C_BUFFER_EVENT_COMPLETE) 
+                {
+                    i2cStates = I2C_DONE;
+                    mikropData.I2CBufferHandle = NULL;
+                }
+                if(mikropData.I2CBufferEvent == DRV_I2C_BUFFER_EVENT_ERROR)
+                {
+                    i2cStates = I2C_ERROR;
+                }
+            }
+            break;
+        }
+		case I2C_DONE:
+        {
+            break;
+        }
+        case I2C_ERROR:
+        {     
+            break;
+        }
+    }
+}
+
 
 bool I2C_Done(COM_TYPE commtype)
 {
